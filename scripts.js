2b5c1ae92725af1dfe3e3fc59a10a51fc8b10e90
jacobbeacom <jdbeacom97@comcast.net>
2021-11-16 20:33:43 -0600

    Added a header to the index.html file as well as three buttons for a crude first attempt at a javascript GUI. Got logic in place for each button to play one round of rock paper scissors using the choice displayed on the button. Results are still logged in console, future commits will contain a scoring system.

--- scripts.js
+++ scripts.js
@@ -8,32 +8,69 @@ function computerPlay() {
 
 let winCount = 0;
 
+
 function singleRound(playerDecision, computerDecision) {
-    playerDecision = prompt("Rock, Paper, or Scissors?");
     playerDecision = playerDecision.toLowerCase();
     computerDecision = computerPlay();
-    if ((playerDecision === "rock" && computerDecision === "Scissors") || (playerDecision === "paper" && computerDecision === "Rock") || (playerDecision === "scissors" && computerDecision === "Paper")) {
+    console.log(computerDecision);
+    if ((playerDecision === "rock" && computerDecision === "Scissors") ||
+             (playerDecision === "paper" && computerDecision === "Rock") || 
+                    (playerDecision === "scissors" && computerDecision === "Paper")) {
         winCount++;
         return "Nice Win!";
     }
     else {
         return "Too bad! You lost.";
     }
-
 }
 
-function game() {
-    console.log(singleRound());
-    console.log(singleRound());
-    console.log(singleRound());
-    console.log(singleRound());
-    console.log(singleRound());
 
-    if (winCount >= 3) {
-        return "Winner: User Loser: Computer"
-    }
-    else {
-        return "Winner: Computer Loser: User"
-    }
+const rock = document.querySelector('#rock');
 
-}
+    rock.addEventListener('click', () => {
+    playerDecision = 'rock';
+    console.log(playerDecision);
+    console.log(singleRound(playerDecision));
+        
+
+    });
+
+
+const paper = document.querySelector('#paper');
+
+    paper.addEventListener('click', () => {
+    playerDecision = 'paper';
+    console.log(playerDecision);
+    console.log(singleRound(playerDecision));    
+
+    });
+
+
+const scissors = document.querySelector('#scissors');
+
+    scissors.addEventListener('click', () => {
+    playerDecision = 'scissors';
+    console.log(playerDecision);
+    console.log(singleRound(playerDecision));
+
+    });
+
+
+
+//The singleRound function repeated 5 times, with results logged to console
+//function game() {
+//    console.log(singleRound());
+//    console.log(singleRound());
+//    console.log(singleRound());
+//    console.log(singleRound());
+//    console.log(singleRound());
+
+//    if (winCount >= 3) {
+//        return "Winner: User Loser: Computer";
+//    }
+//    else {
+//        return "Winner: Computer Loser: User";
+//    }
+//
+//}
+
